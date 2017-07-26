2f7024941d7370b5595a2871742f28d4bce32125
Jason Kulatunga <jk17@ualberta.ca>
2017-07-26 10:11:39 -0700

    added shebang.

--- capsulecd.sh
+++ capsulecd.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # retrieve the latest capsulecd release info
 asset_url=$(curl -s https://api.github.com/repos/AnalogJ/capsulecd/releases/latest \
 	| grep browser_download_url | grep 'capsulecd' | cut -d '"' -f 4)
