3e05d0b9597499b470672847ca26b30bc23d2608
Jason Kulatunga <jason@thesparktree.com>
2021-06-17 15:37:49 -0700

    update packagr binaries.

--- packagr.sh
+++ packagr.sh
@@ -46,19 +46,20 @@ chmod +x packagr-releasr
 ########################################################################################################################
 # Publishr
 ########################################################################################################################
-#RELEASE_URL="https://api.github.com/repos/packagrio/publishr/releases/"
-#if [ "${1}" == "latest" ]; then
-#    RELEASE_URL="${RELEASE_URL}${1}"
-#else
-#    RELEASE_URL="${RELEASE_URL}tags/v${1}"
-#fi
-#
-#asset_url=$(curl -s "${RELEASE_URL}" \
-#	| grep browser_download_url | grep 'packagr-publishr-linux' | cut -d '"' -f 4)
-#
-## download the publishr asset here.
-#echo "curl -L -o packagr-publishr $asset_url"
-#curl -L -o packagr-publishr $asset_url
-#
-## make publishr executable
-#chmod +x packagr-publishr
+
+RELEASE_URL="https://api.github.com/repos/packagrio/publishr/releases/"
+if [ "${1}" == "latest" ]; then
+    RELEASE_URL="${RELEASE_URL}${1}"
+else
+    RELEASE_URL="${RELEASE_URL}tags/v${1}"
+fi
+
+asset_url=$(curl -s "${RELEASE_URL}" \
+	| grep browser_download_url | grep 'packagr-publishr-linux' | cut -d '"' -f 4)
+
+# download the publishr asset here.
+echo "curl -L -o packagr-publishr $asset_url"
+curl -L -o packagr-publishr $asset_url
+
+# make publishr executable
+chmod +x packagr-publishr
