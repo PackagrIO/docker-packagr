855ebec7ac525e67adc2b7ab8340bea93f46e00e
Jason Kulatunga <jason@thesparktree.com>
2021-06-19 08:37:04 -0700

    cleanup function to download assets from github.

--- packagr.sh
+++ packagr.sh
@@ -1,65 +1,27 @@
 #!/usr/bin/env bash
 # retrieve the latest packagr binaries
-########################################################################################################################
-# Bumpr
-########################################################################################################################
 
-export RELEASE_URL="https://api.github.com/repos/packagrio/bumpr/releases/"
-if [ "${1}" == "latest" ]; then
-    export RELEASE_URL="${RELEASE_URL}${1}"
-else
-    export RELEASE_URL="${RELEASE_URL}tags/v${1}"
-fi
+# Download artifacts from github release for a Packagrio repository
+# download_github_release_asset {git_tag/latest} {packagr_tool_name}
+function download_github_release_asset {
+  export RELEASE_URL="https://api.github.com/repos/packagrio/${2}/releases/"
+  if [ "${1}" == "latest" ]; then
+      export RELEASE_URL="${RELEASE_URL}${1}"
+  else
+      export RELEASE_URL="${RELEASE_URL}tags/v${1}"
+  fi
 
-export asset_url=$(curl -s "${RELEASE_URL}" \
-	| grep browser_download_url | grep 'packagr-bumpr-linux' | cut -d '"' -f 4)
+  export asset_url=$(curl -s "${RELEASE_URL}" \
+    | grep browser_download_url | grep "packagr-${2}-linux" | cut -d '"' -f 4)
 
-# download the bumpr asset here.
-echo "curl -L -o packagr-bumpr $asset_url"
-curl -L -o packagr-bumpr $asset_url
+  # download the tool asset here.
+  echo "curl -L -o packagr-${2} $asset_url"
+  curl -L -o "packagr-${2}" $asset_url
 
-# make bumpr executable
-chmod +x packagr-bumpr
+  # make tool executable
+  chmod +x "packagr-${2}"
+}
 
-########################################################################################################################
-# Releasr
-########################################################################################################################
-
-export RELEASE_URL="https://api.github.com/repos/packagrio/releasr/releases/"
-if [ "${1}" == "latest" ]; then
-    export RELEASE_URL="${RELEASE_URL}${1}"
-else
-    export RELEASE_URL="${RELEASE_URL}tags/v${1}"
-fi
-
-export asset_url=$(curl -s "${RELEASE_URL}" \
-	| grep browser_download_url | grep 'packagr-releasr-linux' | cut -d '"' -f 4)
-
-# download the releasr asset here.
-echo "curl -L -o packagr-releasr $asset_url"
-curl -L -o packagr-releasr $asset_url
-
-# make releasr executable
-chmod +x packagr-releasr
-
-
-########################################################################################################################
-# Publishr
-########################################################################################################################
-
-export RELEASE_URL="https://api.github.com/repos/packagrio/publishr/releases/"
-if [ "${1}" == "latest" ]; then
-    export RELEASE_URL="${RELEASE_URL}${1}"
-else
-    export RELEASE_URL="${RELEASE_URL}tags/v${1}"
-fi
-
-export asset_url=$(curl -s "${RELEASE_URL}" \
-	| grep browser_download_url | grep 'packagr-publishr-linux' | cut -d '"' -f 4)
-
-# download the publishr asset here.
-echo "curl -L -o packagr-publishr $asset_url"
-curl -L -o packagr-publishr $asset_url
-
-# make publishr executable
-chmod +x packagr-publishr
+download_github_release_asset "latest" "bumpr"
+download_github_release_asset "latest" "releasr"
+download_github_release_asset "latest" "publishr"
